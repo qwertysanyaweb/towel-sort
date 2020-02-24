6cf7d012d5a3624c9064d3ffbd137ec4bd3c49f7
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-02-24 12:14:10 +0500

    Update scripts
    wrote a creak

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+	if(matrix){
+		var temp;
+		var newarrey = [];
+
+		for (var i = 0; i < matrix.length; i++) {
+			if(i % 2){
+				matrix[i].reverse();
+			}
+		}
+		for (var i = 0; i < matrix.length; i++) {
+			if(typeof(matrix[i]) == 'object'){
+				for (var b = 0; b < matrix[i].length; b++) {
+					newarrey.push(matrix[i][b]);
+				}
+			}else{
+				newarrey.push(matrix[i][b]);
+			}
+		}
+		return newarrey;
+	}else{
+		return [];
+	}
 }
